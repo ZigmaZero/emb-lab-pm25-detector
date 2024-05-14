61bb708fd596be580013b1935801576564050d29
Blazxex <armygm162@gmail.com>
2024-05-14 13:58:35 +0700

    add margin

--- project-frontend/src/App.vue
+++ project-frontend/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="landingPage min-h-screen flex flex-col justify-between relative">
     <div class="grid grid-cols-1 md:grid-cols-6 gap-4 px-4 flex-grow-0">
-      <div class="col-span-1 md:col-span-6 m-0">
+      <div class="col-span-1 md:col-span-6 mt-3">
         <HeaderField :town="town" :AQI="AQI" />
       </div>
-      <div class="col-span-1 md:col-span-2 h-36 md:h-44">
+      <div class="col-span-1 md:col-span-2 h-36 md:h-48">
         <MapField :center="center" :markers="markers" />
       </div>
-      <div class="col-span-1 md:col-span-2 h-36 md:h-44">
+      <div class="col-span-1 md:col-span-2 h-36 md:h-48">
         <PmField :AQI="AQI" />
       </div>
-      <div class="col-span-1 md:col-span-2 h-36 md:h-44">
+      <div class="col-span-1 md:col-span-2 h-36 md:h-48">
         <CarbonField :CO2="CO2" />
       </div>
-      <div class="col-span-1 md:col-span-6 h-36 md:h-44 mb-10">
+      <div class="col-span-1 md:col-span-6 h-36 md:h-48 mb-10">
         <ChartField />
       </div>
     </div>
