dc338dff3a759ba1d7d0d6eef3cdfdd68315aad7
Blazxex <82823435+blazxex@users.noreply.github.com>
2024-05-20 16:47:58 +0700

    update info page

--- project-frontend/src/components/InfoPage.vue
+++ project-frontend/src/components/InfoPage.vue
@@ -7,23 +7,35 @@
         <h2 class="text-xl font-bold">Project Information</h2>
         <button @click="$emit('close')" class="text-black">&times;</button>
       </div>
-      <p class="mt-4">
-        This website is part of the final project for the
-        <b>2110366 Embedded Systems Lab</b> in Computer Engineering at
-        Chulalongkorn University. We utilize the WHO AQI guidelines as a
-        reference for determining hazardous air quality levels. Please note that
-        the data on this site may include mock data for educational purposes.
+      <div class="p-2">
+        <p>
+          This website is part of the final project for the
+          <b>2110366 Embedded Systems Lab</b> in Computer Engineering at
+          Chulalongkorn University. <br /><br />
+          This project is implemented to be part of
+          <a
+            href="https://web.archive.org/web/20231229111820/https://www.chadchart.com/policy/6215ef934e43cd8b4760bc17"
+            ><b>ChatChat's Bangkok policy</b></a
+          >. We utilize the WHO AQI guidelines as a reference for determining
+          hazardous air quality levels. <u>Please note</u> that the data on this
+          site may include mock data for educational purposes.
+        </p>
+
         <br />
+
+        <p>
+          This interface displays data that we collect from GPS, PM2.5, and CO2
+          sensors connected to an STM32-based NodeMCU. The data is transmitted
+          via NETPIE for visualization here.
+        </p>
         <br />
-        This interface displays data that we collect from GPS, PM2.5, and CO2
-        sensors connected to an STM32-based NodeMCU. The data is transmitted via
-        NETPIE for visualization here.
-        <br />
-        <br />
-        <b>Location Data Usage</b>: Your location data will be used solely for
-        displaying relevant data and will not be collected or stored on our
-        servers.
-      </p>
+
+        <p>
+          <b>Location Data Usage</b>: Your location data will be used solely for
+          displaying relevant data and will not be collected or stored on our
+          servers.
+        </p>
+      </div>
     </div>
   </div>
 </template>
