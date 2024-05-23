7efd7e9a11427d6c8deb97dbf8678b16286d5d12
Blazxex <armygm162@gmail.com>
2024-05-23 19:46:39 +0700

    change token

--- project-frontend/src/APIConfig.js
+++ project-frontend/src/APIConfig.js
@@ -1,11 +1,10 @@
 export async function getCurrentData() {
   const url = "https://api.netpie.io/v2/feed/api/v1/datapoints/query";
-  const UserToken =
-    "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJHUnBpZUdFSCIsImlhdCI6MTcxNjQ2NzMwOCwibmJmIjoxNzE2NDY3MDA4LCJleHAiOjE3MTY1NTM3MDgsImV4cGlyZUluIjo4NjQwMCwiY3R4Ijp7ImNsaWVudGlkIjoiNWI0NGM0ZDRkMWMwODY5ZmE0YjNlZDkyZTFmNzgzYjgiLCJ1c2VyaWQiOiJVNzQ0ODAzMTU5MzAxIn0sImlzcyI6ImNlcjp1c2VydG9rZW4ifQ.x7ozHZrY7BbGO29uh1geNRx_fZ4D4MlO_sLmgMqYl6LYyrytx8Ig0OFlnV7py85h2MKB9IAjWV13xK_A0h5vgg";
+  const UserToken = import.meta.env.VITE_NET_USER_TOKEN;
   const response = await fetch(url, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${UserToken}`,
+      Authorization: `Device ${UserToken}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
@@ -64,7 +63,7 @@ export async function getGraph() {
   const response = await fetch(url, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${UserToken}`,
+      Authorization: `Device ${UserToken}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
@@ -99,46 +98,3 @@ export async function getGraph() {
     console.error("Failed to retrieve Graph data", response.statusText);
   }
 }
-
-export async function checkToken() {
-  const url = "https://api.netpie.io/v2/feed/api/v1/datapoints/query";
-  const UserToken = import.meta.env.VITE_NET_USER_TOKEN;
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${UserToken}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      start_relative: {
-        value: 1,
-        unit: "days",
-      },
-      metrics: [
-        {
-          name: "1af58697-e327-49e6-8d6e-59cb9ba6d6fb",
-          order: "desc",
-          limit: 50,
-          tags: {
-            attr: ["pm25.level"],
-          },
-        },
-      ],
-    }),
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    let pm25 = [];
-    try {
-      pm25 = data.queries[0].results.map((result) => result.values);
-    } catch (e) {
-      console.error("Error retrieving pm2.5 Graph data", e);
-    }
-    // console.log(pm25[0]);
-    return pm25[0];
-  } else {
-    console.error("Failed to retrieve Graph data", response.statusText);
-  }
-}
-getCurrentData();
